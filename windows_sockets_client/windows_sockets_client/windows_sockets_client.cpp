c1e37d2065a397c500d80618f41de2f7351d8895
FilipSzweda <filip.szweda1@gmail.com>
2022-04-23 22:03:16 +0200

    Send sender and receiver data to server, refactor client and server

--- windows_sockets_client/windows_sockets_client/windows_sockets_client.cpp
+++ windows_sockets_client/windows_sockets_client/windows_sockets_client.cpp
@@ -1,50 +1,66 @@
-#include<io.h>
-#include<iostream>
-#include<stdio.h>
-#include<winsock2.h>
+#include <io.h>
+#include <iostream>
+#include <stdio.h>
+#include <winsock2.h>
 #include <windows.h>
 #include <thread>
+#include <mutex>
+#include <string>
 
 #pragma warning(disable:4996) 
 
 #pragma comment(lib,"ws2_32.lib")
 
 #define ST_PORT 8080
-#define CLIENTS_NUMBER 2
+#define CLIENTS_NUMBER 3
 
-void create_client() {
-    SOCKET s;
-    struct sockaddr_in sa;
+std::mutex mtx;
+
+void create_client(std::string userNumber) {
     WSADATA wsas;
-    WORD wersja;
-    wersja = MAKEWORD(2, 0);
-    WSAStartup(wersja, &wsas);
-    s = socket(AF_INET, SOCK_STREAM, 0);
-    memset((void*)(&sa), 0, sizeof(sa));
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(ST_PORT);
-    sa.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    int result;
-    result = connect(s, (struct sockaddr FAR*) & sa, sizeof(sa));
-    if (result == SOCKET_ERROR) {
-        printf("Connection error\n");
+    WORD version;
+    version = MAKEWORD(2, 0);
+    if (int result = WSAStartup(version, &wsas)) {
+        std::cout << "Winsock loading error\n";
     }
 
-    int dlug;
-    char buf[80];
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in clientAddress;
+    memset((void*)(&clientAddress), 0, sizeof(clientAddress));
+    clientAddress.sin_family = AF_INET;
+    clientAddress.sin_port = htons(ST_PORT);
+    clientAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (int result = connect(clientSocket, (struct sockaddr FAR*) & clientAddress, sizeof(clientAddress)) == SOCKET_ERROR) {
+        std::cout << "Connection error\n";
+    }
+    
+    std::string buffer;
+    std::string message;
+    std::string receiverNumber;
     while (true) {
-        fgets(buf, 80, stdin);
-        dlug = strlen(buf); buf[dlug - 1] = '\0';
-        send(s, buf, dlug, 0);
-        if (strcmp(buf, "quit") == 0) break;
+        mtx.lock();
+        std::cout << "[User n. " << userNumber << "] Message:\n";
+        std::cin >> message;
+        std::cout << "[User n. " << userNumber << "] Receiver (if everyone: 0):\n";
+        std::cin >> receiverNumber;
+        mtx.unlock();
+
+        buffer = userNumber + receiverNumber + message;
+        send(clientSocket, buffer.c_str(), buffer.length() + 1, 0);
     }
-    closesocket(s);
+
+    closesocket(clientSocket);
     WSACleanup();
 }
 
 int main() {
-    std::thread clients[CLIENTS_NUMBER] {std::thread {create_client}, std::thread {create_client}};
+    std::thread clients[CLIENTS_NUMBER];
+
+    for (int clientNumber = 0; clientNumber < CLIENTS_NUMBER; clientNumber++) {
+        clients[clientNumber] = std::thread{ create_client, std::to_string(clientNumber + 1) };
+    }
 
     for (int clientNumber = 0; clientNumber < CLIENTS_NUMBER; clientNumber++) {
         clients[clientNumber].join();
--- windows_sockets_server/windows_sockets_server/windows_sockets_server.cpp
+++ windows_sockets_server/windows_sockets_server/windows_sockets_server.cpp
@@ -6,15 +6,19 @@
 #pragma comment(lib,"ws2_32.lib")
 
 #define ST_PORT 8080
+#define CLIENTS_NUMBER 3
 
 int main() {
     WSADATA wsas;
+    WORD version;
+    version = MAKEWORD(1, 1);
+    if (int result = WSAStartup(version, &wsas)) {
+        std::cout << "Winsock loading error\n";
+    }
+
     int result;
-    WORD wersja;
-    wersja = MAKEWORD(1, 1);
-    result = WSAStartup(wersja, &wsas);
-    SOCKET s;
-    s = socket(AF_INET, SOCK_STREAM, 0);
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in sa;
     memset((void*)(&sa), 0, sizeof(sa));
@@ -22,20 +26,25 @@ int main() {
     sa.sin_port = htons(ST_PORT);
     sa.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    result = bind(s, (struct sockaddr FAR*) & sa, sizeof(sa));
-    result = listen(s, 5);
+    if (int result = bind(serverSocket, (struct sockaddr FAR*) & sa, sizeof(sa))) {
+        std::cout << "Binding error\n";
+    }
 
-    SOCKET si[2];
+    if (int result = listen(serverSocket, 5)) {
+        std::cout << "Listening error\n";
+    }
+
+    SOCKET si[CLIENTS_NUMBER];
     struct sockaddr_in sc;
     int lenc;
     while(true) {
         lenc = sizeof(sc);
-        for (int i = 0; i < 2; i++) {
-            si[i] = accept(s, (struct sockaddr FAR*) & sc, &lenc);
+        for (int i = 0; i < CLIENTS_NUMBER; i++) {
+            si[i] = accept(serverSocket, (struct sockaddr FAR*) & sc, &lenc);
         }
         
         while (true) {
-            for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < CLIENTS_NUMBER; i++) {
                 char buf[80];
                 if (recv(si[i], buf, 80, 0) > 0) {
                     if (strcmp(buf, "quit") == 0) {
@@ -47,8 +56,6 @@ int main() {
                 };
             }
         }
-
-        
     }
 
     return 0;
