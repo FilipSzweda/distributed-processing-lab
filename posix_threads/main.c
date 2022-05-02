56c82508702f408f966657dc4cbbc984e5841dde
FilipSzweda <filip.szweda1@gmail.com>
2022-05-02 12:03:34 +0200

    Finish posix threads

--- posix_threads/main.c
+++ posix_threads/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
-#include <mutex>
 
 #define INTERVAL_END 100
 #define NUMBERS_COUNT (INTERVAL_END - 1)
 #define THREADS_COUNT 4
 
 int numbers[NUMBERS_COUNT] = {};
-std::mutex mtx;
+
+pthread_mutex_t mtx;
 
 struct args {
     int startIndex;
@@ -15,9 +16,15 @@ struct args {
 };
 
 void *eratosthenes_sieve(void *arguments) {
-    struct args interval = (struct args*) arguments;
-    for(int i = interval.startIndex; i <= interval.endIndex; i++) {
-        int number = numbers[i];
+    struct args* interval = (struct args*) arguments;
+    for(int i = interval->startIndex; i < interval->endIndex; i++) {
+        for(int j = 0; j < NUMBERS_COUNT; j++) {
+            pthread_mutex_lock(&mtx);
+            if (numbers[j] != numbers[i] && numbers[j] % numbers[i] == 0) {
+                numbers[j] = INTERVAL_END + 1;
+            }
+            pthread_mutex_unlock(&mtx);
+        }
     }
 }
 
@@ -27,18 +34,26 @@ int main() {
     }
 
     pthread_t threads[THREADS_COUNT];
+    struct args* intervals = (struct args*)malloc(THREADS_COUNT * sizeof(struct args));
 
     for(int i = 0; i < THREADS_COUNT; i++) {
-        // tutaj podać przedział w args do pthread_create
-        // za 10 pkt. uzywac mutexa do wpisywania do tablicy (tablica wspolna)
-        int startIndex = i * NUMBERS_COUNT / 2;
-        int endIndex = (i + 1) * NUMBERS_COUNT / 2;
-        struct args interval {startIndex, endIndex};
-        pthread_create(&threads[i], NULL, eratosthenes_sieve, (void*)&interval);
+        int startIndex = i * NUMBERS_COUNT / THREADS_COUNT;
+        int endIndex = (i + 1) * NUMBERS_COUNT / THREADS_COUNT;
+        intervals[i].startIndex = startIndex;
+        intervals[i].endIndex = endIndex;
+        pthread_create(&threads[i], NULL, eratosthenes_sieve, (void*)&intervals[i]);
     }
 
     for(int i = 0; i < THREADS_COUNT; i++) {
         pthread_join(threads[i], NULL);
+    }
+
+    free(intervals);
+
+    for(int i = 0; i < NUMBERS_COUNT; i++) {
+        if(numbers[i] != INTERVAL_END + 1) {
+            printf("%i, ", numbers[i]);
+        }
     }
 
     return 0;
