ce6650e72cdb898ad9fe876ba47c3bcf4a2287a6
FilipSzweda <filip.szweda1@gmail.com>
2022-05-02 12:56:08 +0200

    Make posix threads more in line with the task

--- posix_threads/main.c
+++ posix_threads/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
 #define INTERVAL_END 100
 #define NUMBERS_COUNT (INTERVAL_END - 1)
 #define THREADS_COUNT 4
 
 int numbers[NUMBERS_COUNT] = {};
-
+int primes[NUMBERS_COUNT] = {};
+int curr_p = 0;
 pthread_mutex_t mtx;
 
 struct args {
@@ -15,15 +17,31 @@ struct args {
     int endIndex;
 };
 
+int remove_element(int*from, int total, int index) {
+    if((total - index - 1) > 0) {
+        memmove(from + index, from + index + 1, sizeof(int) * (total - index - 1));
+    }
+    return total - 1;
+}
+
 void *eratosthenes_sieve(void *arguments) {
     struct args* interval = (struct args*) arguments;
     for(int i = interval->startIndex; i < interval->endIndex; i++) {
-        for(int j = 0; j < NUMBERS_COUNT; j++) {
+        if(numbers[i] != INTERVAL_END + 1) {
             pthread_mutex_lock(&mtx);
+            primes[curr_p] = numbers[i];
+            curr_p += 1;
+            pthread_mutex_unlock(&mtx);
+        }
+        for(int j = 0; j < NUMBERS_COUNT; j++) {
             if (numbers[j] != numbers[i] && numbers[j] % numbers[i] == 0) {
+                for(int k = 0; k < curr_p; k++) {
+                    if(primes[k] == numbers[j]) {
+                        curr_p = remove_element(primes, curr_p, k);
+                    }
+                }
                 numbers[j] = INTERVAL_END + 1;
             }
-            pthread_mutex_unlock(&mtx);
         }
     }
 }
@@ -50,11 +68,13 @@ int main() {
 
     free(intervals);
 
+    printf("curr_p: %i\n", curr_p);
     for(int i = 0; i < NUMBERS_COUNT; i++) {
         if(numbers[i] != INTERVAL_END + 1) {
             printf("%i, ", numbers[i]);
         }
     }
+    printf("\n");
 
     return 0;
 }
